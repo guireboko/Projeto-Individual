d4e51c5ecddda4931e946c36be397da8f13b2d69
guireboko <guilherme.rferreira@sptech.school>
2024-11-17 17:31:14 -0300

    Login e Cadastro

--- site/app.js
+++ site/app.js
@@ -10,18 +10,16 @@ var app = express();
 
 var indexRouter = require("./src/routes/index");
 var usuarioRouter = require("./src/routes/usuarios");
-var musicaRouter = require("./src/routes/musicas");
 
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(cors());
 
 app.use("/", indexRouter);
 app.use("/usuarios", usuarioRouter);
-app.use("/musica", musicaRouter);
 
 app.listen(PORTA, function () {
     console.log(`Servidor do seu site já está rodando! Acesse o caminho a seguir para visualizar: http://localhost:${PORTA} \n
