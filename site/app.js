f5c0a5e6bacabe4d1e1ff335cf5a07aad7dec8bd
guireboko <guilherme.rferreira@sptech.school>
2024-11-20 16:34:03 -0300

    dashboard

--- site/app.js
+++ site/app.js
@@ -4,11 +4,11 @@ process.env.AMBIENTE_PROCESSO = "desenvolvimento";
 var express = require("express");
 var cors = require("cors");
 var path = require("path");
-var PORTA = process.env.AMBIENTE_PROCESSO == "desenvolvimento" ? 3333 : 8080;
+var PORTA = process.env.AMBIENTE_PROCESSO == "desenvolvimento" ? 3300 : 8080;
 
 var app = express();
 
-var indexRouter = require("./src/routes/index");
+
 var usuarioRouter = require("./src/routes/usuarios");
 
 
@@ -18,7 +18,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(cors());
 
-app.use("/", indexRouter);
 app.use("/usuarios", usuarioRouter);
 
 app.listen(PORTA, function () {
