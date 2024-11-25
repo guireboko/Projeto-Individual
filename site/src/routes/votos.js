063812e6a8fca54d7a90548a8dcd98bf7ab57efc
guireboko <guilherme.rferreira@sptech.school>
2024-11-25 13:32:12 -0300

    img

--- site/src/routes/votos.js
+++ site/src/routes/votos.js
@@ -4,15 +4,15 @@ var router = express.Router();
 var votoController = require("../controllers/votoController");
 
 router.post("/votar/:nomeJogador", function (req, res) {
-  votoController.votarPorNome(req, res); // invoca a função da controller para "incrementar votos por nome do jogador"
+  votoController.votarPorNome(req, res); 
 });
 
 router.get("/ultimos", function (req, res) {
-  votoController.obterVotosAtualizados(req, res); // invoca a função da controller para "obter os votos atualizados"
+  votoController.obterVotosAtualizados(req, res); 
 });
 
 router.get("/buscarPorQuantidade", function (req, res) {
-  votoController.buscarVotosQuantidade(req, res); // invoca a função da controller para "obter os votos atualizados"
+  votoController.buscarVotosQuantidade(req, res); 
 });
 
 
